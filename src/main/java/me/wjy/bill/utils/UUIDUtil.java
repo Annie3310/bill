7e1ed4b28a6826f65728b1c003ec880d166b6650
王金义 <jinyi.wang@liangyihui.net>
2021-08-03 11:09:28 +0800

    Add: 添加 UUIDUtil

---
+++ src/main/java/me/wjy/bill/utils/UUIDUtil.java
@@ -0,0 +1,25 @@
+package me.wjy.bill.utils;
+
+import java.util.UUID;
+
+/**
+ * @author 王金义
+ * @date 2021/8/3
+ */
+public class UUIDUtil {
+    public static String getUUID() {
+        return UUID.randomUUID().toString().replace("-","");
+    }
+
+    public static String getUUID(int len) {
+        if (0 >= len) {
+            return null;
+        }
+        String uuid = getUUID();
+        StringBuilder stringBuilder = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            stringBuilder.append(uuid.charAt(i));
+        }
+        return stringBuilder.toString();
+    }
+}
