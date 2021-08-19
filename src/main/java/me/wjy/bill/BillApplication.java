f6b1b52ce09e331b963ddf7528103849a8fee236
王金义 <jinyi.wang@liangyihui.net>
2021-08-19 14:59:51 +0800

    - 删除一条已完成的 TODO 注释 - 更新文档

--- src/main/java/me/wjy/bill/BillApplication.java
+++ src/main/java/me/wjy/bill/BillApplication.java
@@ -16,7 +16,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class BillApplication {
 
     // TODO 封装 Redis
-    // TODO 限制请求 get_token 接口的次数 (请求该接口无需携带 Token, 也就不能以 userId 作为限制标准)
 
     public static void main(String[] args) {
         SpringApplication.run(BillApplication.class, args);
